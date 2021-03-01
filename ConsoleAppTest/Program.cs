adab457808f3941bec31df4269b9a8d243b03a09
EbarriosCode <guayoswing@gmail.com>
2021-02-28 22:42:53 -0600

    Computer Science Problems

---
+++ ConsoleAppTest/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Implement the FindMaxSum method that, efficiently with respect to time used, returns the largest sum of any two elements in the given list of positive numbers.
+        /// For example, the largest sum of the list {5, 9, 7, 11} is the sum of the elements 9 and 11, wich is 20.        
+        /// </summary>
+        
+        static void Main(string[] args)
+        {            
+            List<int> arr = new List<int> { 52, 12, 34, 10, 6, 40, 12, 40, 100 };
+            Console.WriteLine(FindMaxSum(arr));            
+        }
+
+        static int FindMaxSum(List<int> list)
+        {
+            // Ordenamiento por QuickSort
+            QuickSort(0, list.Count - 1, list);
+
+            // Buscar los dos últimos índices, es en donde estan los valores mayores
+            int result = 0;
+            int length = list.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == length-1)
+                    result = list[i] + list[i - 1];
+            }
+
+            return result;
+        }
+
+        // Algoritmo QuickSort
+        static void QuickSort(int inicio, int fin, List<int> list)
+        {
+            int indexPivote;
+
+            // Caso base, un elemento o fragmento inválido
+            if (inicio >= fin)
+                return;
+
+            // Obtener el indice del pivote para el fragmento actual con el que estamos trabajando
+            indexPivote = Particion(inicio, fin, list);
+
+            // Casos Inductivos
+            QuickSort(inicio, indexPivote - 1, list); // Fragmento de la izquierda
+            QuickSort(indexPivote + 1, fin, list);    // Fragmento de la derecha
+        }
+
+        static int Particion(int inicio, int fin, List<int> list)
+        {
+            int pivote;
+            int indexPivote;
+            int iteracion;
+           
+            pivote = list[fin];            
+            indexPivote = inicio;
+            
+            for (iteracion = inicio; iteracion < fin; iteracion++)
+            {                
+                if (list[iteracion] <= pivote)
+                {                    
+                    Swap(iteracion, indexPivote, list);                    
+                    indexPivote++;
+                }
+            }
+            
+            Swap(indexPivote, fin, list);
+
+            return indexPivote;
+        }
+
+        static void Swap(int index1, int index2, List<int> list)
+        {
+            int temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
---
+++ ConsoleAppTestTwo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppTestTwo
+{
+    class Program
+    {
+        /// <summary>
+        /// Write a method that finds, efficiently with respect to time used, all numbers that occur exactly once in the input collection.
+        /// For example, FindUniqueNumbers(new int[] { 1, 2, 1, 3 }) should return {2, 3}
+        /// </summary>        
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 1, 3, 3, 4, 5, 6, 6, 2, 100, 99, 99 };
+            foreach (var number in FindUniqueNumbers(numbers))
+                Console.WriteLine(number);
+
+        }
+
+        public static IEnumerable<int> FindUniqueNumbers(IEnumerable<int> numbers)
+        {
+            List<int> result = new();
+            QuickSort(0, numbers.Count() - 1, numbers as int[]);            
+            int n = numbers.Count();
+
+            if (numbers.ElementAt(0) != numbers.ElementAt(1))            
+                result.Add(numbers.ElementAt(0));                            
+
+            for (int i = 1; i < n - 1; i++)
+            {
+                if (numbers.ElementAt(i) != numbers.ElementAt(i + 1) && numbers.ElementAt(i) != numbers.ElementAt(i - 1))                
+                    result.Add(numbers.ElementAt(i));                                    
+            }
+            
+            if (numbers.ElementAt(n - 2) != numbers.ElementAt(n - 1))
+            {
+                result.Add(numbers.ElementAt(n - 1));                
+            }
+
+            return result;
+        }
+
+        // Algoritmo QuickSort
+        static void QuickSort(int inicio, int fin, int[] list)
+        {
+            int indexPivote;
+
+            // Caso base, un elemento o fragmento inválido
+            if (inicio >= fin)
+                return;
+
+            // Obtener el indice del pivote para el fragmento actual con el que estamos trabajando
+            indexPivote = Particion(inicio, fin, list);
+
+            // Casos Inductivos
+            QuickSort(inicio, indexPivote - 1, list); // Fragmento de la izquierda
+            QuickSort(indexPivote + 1, fin, list);    // Fragmento de la derecha
+        }
+
+        static int Particion(int inicio, int fin, int[] list)
+        {
+            int pivote;
+            int indexPivote;
+            int iteracion;
+
+            pivote = list[fin];
+            indexPivote = inicio;
+
+            for (iteracion = inicio; iteracion < fin; iteracion++)
+            {
+                if (list[iteracion] <= pivote)
+                {
+                    Swap(iteracion, indexPivote, list);
+                    indexPivote++;
+                }
+            }
+
+            Swap(indexPivote, fin, list);
+
+            return indexPivote;
+        }
+
+        static void Swap(int index1, int index2, int[] list)
+        {
+            int temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
